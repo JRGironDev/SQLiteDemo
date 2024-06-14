ae5a3b34ad711beee41f08269931b78ea4d7f302
José Quiñónez <joserobertoq@outlook.com>
2024-06-14 08:35:58 -0600

    Add Customer Repository Methods

--- SQLiteDemo/Repositories/CustomerRepository.cs
+++ SQLiteDemo/Repositories/CustomerRepository.cs
@@ -3,17 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteDemo.MVVM.Models;
 
 namespace SQLiteDemo.Repositories
 {
     public class CustomerRepository
     {
         SQLiteConnection connection;
+        public string StatusMessage { get; set; }
 
         public CustomerRepository()
         {
             connection = new SQLiteConnection(Constants.DatabasePath, Constants.Flags);
             
+            connection.CreateTable<Customer>();
+        }
+
+        public void AddOrUpdate(Customer customer)
+        {
+            int result = 0;
+
+            try
+            {
+                if(customer.ID != 0)
+                {
+                    result = connection.Update(customer);
+                    StatusMessage = $"{result} record(s) updated [Name: {customer.Name}]";
+                }
+                else
+                {
+                    result = connection.Insert(customer);
+                    StatusMessage = $"{result} record(s) added [Name: {customer.Name}]";
+                }
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to add {customer.Name}. Error: {ex.Message}";
+            }
+        }
+    
+        public List<Customer> GetAll()
+        {
+            try
+            {
+                return connection.Table<Customer>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to retrieve data. Error: {ex.Message}";
+            }
+
+            return null;
+        }
+    
+        public Customer GetById(int id)
+        {
+            try
+            {
+                return connection.Table<Customer>().FirstOrDefault(c => c.ID == id);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to retrieve data. Error: {ex.Message}";
+            }
+
+            return null;
+        }
+
+        public List<Customer> GetAll2()
+        {
+            try
+            {
+                return connection.Query<Customer>("SELECT * FROM Customers").ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to retrieve data. Error: {ex.Message}";
+            }
+
+            return null;
+        }
+
+        public void Delete(int customerId)
+        {
+            try
+            {
+                var customer = GetById(customerId);
+                int resulconnection.Delete<Customer>(customer);
+                StatusMessage = $"{result} record(s) deleted";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Failed to delete record. Error: {ex.Message}";
+            }
         }
     }
 }
